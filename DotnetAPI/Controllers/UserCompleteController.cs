7b3bffcf514435a4ee9b01716c12d05a299e7b09
lamw1 <wing.lam2@uhs.nhs.uk>
2024-09-10 10:59:59 +0100

    Completed 120 User Controller DynamicParameters

--- DotnetAPI/Controllers/UserCompleteController.cs
+++ DotnetAPI/Controllers/UserCompleteController.cs
@@ -1,5 +1,6 @@
+using System.Data;
+using Dapper;
 using DotnetAPI.Data;
-using DotnetAPI.DTOs;
 using DotnetAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,20 +27,27 @@ public class UserCompleteController : ControllerBase
     public IEnumerable<UserComplete> GetUsers(int userId, bool isActive)
     {
         string sql = @"EXEC TutorialAppSchema.spUsers_Get";
-        string parameters = "";
+        string stringParameters = "";
+        DynamicParameters sqlParameters = new();
 
         if (userId != 0) 
         {
-            parameters += ", @UserId=" + userId.ToString();
+            stringParameters += ", @UserId=@UserIdParameter";
+            sqlParameters.Add("@UserIdParameter", userId, DbType.Int32);
         }
+
         if (isActive)
         {
-            parameters += ", @Active=" + isActive.ToString();
+            stringParameters += ", @Active=@ActiveParameter";
+            sqlParameters.Add("@ActiveParameter", isActive, DbType.Boolean);
         }
 
-        sql += parameters.Substring(1);
+        if (stringParameters.Length > 0)
+        {
+            sql += stringParameters.Substring(1);
+        }
 
-        IEnumerable<UserComplete> users = _dapper.LoadData<UserComplete>(sql);
+        IEnumerable<UserComplete> users = _dapper.LoadDataWithParameters<UserComplete>(sql, sqlParameters);
         return users;
     }
 
@@ -48,19 +56,31 @@ public class UserCompleteController : ControllerBase
     {
         string sql = @"
         EXEC [TutorialAppSchema].[spUser_Upsert]
-            @FirstName = '" + user.FirstName +
-            "', @LastName = '" + user.LastName +
-            "', @Email = '" + user.Email +
-            "', @Gender = '" + user.Gender +
-            "', @Active = '" + user.Active +
-            "', @JobTitle = '" + user.JobTitle +
-            "', @Department = '" + user.Department +
-            "', @Salary = '" + user.Salary +
-            "', @UserId =" + user.UserId.ToString();
+            @FirstName = @FirstNameParameter,
+            @LastName = @LastNameParameter,
+            @Email = @EmailParameter,
+            @Gender = @GenderParameter,
+            @Active = @ActiveParameter,
+            @JobTitle = @JobTitleParameter,
+            @Department = @DepartmentParameter,
+            @Salary = @SalaryParameter,
+            @UserId = @UserIdParameter";
 
 Console.WriteLine(sql);
 
-        if(_dapper.ExecuteSql(sql))
+        DynamicParameters sqlParameters = new();
+
+        sqlParameters.Add("@FirstNameParameter", user.FirstName, DbType.String);
+        sqlParameters.Add("@LastNameParameter", user.LastName, DbType.String);
+        sqlParameters.Add("@EmailParameter", user.Email, DbType.String);
+        sqlParameters.Add("@GenderParameter", user.Gender, DbType.String);
+        sqlParameters.Add("@ActiveParameter", user.Active, DbType.Boolean);
+        sqlParameters.Add("@JobTitleParameter", user.JobTitle, DbType.String);
+        sqlParameters.Add("@DepartmentParameter", user.Department, DbType.String);
+        sqlParameters.Add("@SalaryParameter", user.Salary, DbType.Decimal);
+        sqlParameters.Add("@UserIdParameter", user.UserId, DbType.Int32);
+
+        if (_dapper.ExecuteSqlWithParameters(sql, sqlParameters))
             return Ok();
 
         throw new Exception("Failed to update user");
@@ -70,9 +90,12 @@ Console.WriteLine(sql);
     public IActionResult DeleteUser(int userId)
     {
         string sql = @"TutorialAppSchema.spUser_Delete
-            @UserId = " + userId.ToString();
+            @UserId = @UserIdParameter";
 
-        if (_dapper.ExecuteSql(sql))
+        DynamicParameters sqlParameters = new DynamicParameters();
+        sqlParameters.Add("@UserIdParameter", userId, DbType.Int32);
+
+        if (_dapper.ExecuteSqlWithParameters(sql, sqlParameters))
             return Ok();
 
         throw new Exception("Failed to delete user");
